7c5889e971fc8b299ba6deca89a73819ebb62592
lch9527 <38733164+lch9527@users.noreply.github.com>
2023-02-02 15:31:52 -0800

    save1

---
+++ Source/GoeaTechEval/CustomMovementComponent.cpp
@@ -0,0 +1,99 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CustomMovementComponent.h"
+#include "GoeaTechEvalCharacter.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/Character.h"
+#include "Net/UnrealNetwork.h"
+
+
+
+bool UCustomMovementComponent::FSavedMove_Custom::CanCombineWith(const FSavedMovePtr& NewMove, ACharacter* InCharacter, float MaxDelta) const
+{
+	const FSavedMove_Custom* NewCustomMove = static_cast<FSavedMove_Custom*>(NewMove.Get());
+
+	if (Saved_ToClimb != NewCustomMove->Saved_ToClimb)
+	{
+		return false;
+	}
+
+
+	return FSavedMove_Character::CanCombineWith(NewMove, InCharacter, MaxDelta);
+}
+
+void UCustomMovementComponent::FSavedMove_Custom::Clear()
+{
+	FSavedMove_Character::Clear();
+	Saved_ToClimb = 0;
+}
+
+uint8 UCustomMovementComponent::FSavedMove_Custom::GetCompressedFlags() const
+{
+
+	uint8 Result = FSavedMove_Character::GetCompressedFlags();
+
+	if (Saved_ToClimb) Result |= FLAG_Custom_0;
+
+	return Result;
+}
+
+void UCustomMovementComponent::FSavedMove_Custom::SetMoveFor(ACharacter* C, float InDeltaTime, FVector const& NewAccel, FNetworkPredictionData_Client_Character& ClientData)
+{
+	FSavedMove_Character::SetMoveFor(C, InDeltaTime, NewAccel, ClientData);
+
+	const UCustomMovementComponent* CharacterMovement = Cast<UCustomMovementComponent>(C->GetCharacterMovement());
+
+
+	Saved_ToClimb = CharacterMovement->Safe_ToClimb;
+
+}
+
+void UCustomMovementComponent::FSavedMove_Custom::PrepMoveFor(ACharacter* C)
+{
+	FSavedMove_Character::PrepMoveFor(C);
+
+	UCustomMovementComponent* CharacterMovement = Cast<UCustomMovementComponent>(C->GetCharacterMovement());
+
+	CharacterMovement->Safe_ToClimb = Saved_ToClimb;
+
+}
+
+UCustomMovementComponent::FNetworkPredictionData_Client_Custom::FNetworkPredictionData_Client_Custom(const UCharacterMovementComponent& ClientMovement)
+: Super(ClientMovement)
+{
+
+}
+/*
+UZippyCharacterMovementComponent::FNetworkPredictionData_Client_Zippy::FNetworkPredictionData_Client_Zippy(const UCharacterMovementComponent& ClientMovement)
+: Super(ClientMovement)
+{
+}
+*/
+
+FSavedMovePtr UCustomMovementComponent::FNetworkPredictionData_Client_Custom::AllocateNewMove()
+{
+	//FSavedMove_Custom
+	return FSavedMovePtr(new FSavedMove_Custom());
+}
+
+UCustomMovementComponent::UCustomMovementComponent()
+{
+	NavAgentProps.bCanCrouch = true;
+}
+
+FNetworkPredictionData_Client* UCustomMovementComponent::GetPredictionData_Client() const
+{
+	check(PawnOwner != nullptr)
+
+		if (ClientPredictionData == nullptr)
+		{
+			UCustomMovementComponent* MutableThis = const_cast<UCustomMovementComponent*>(this);
+
+			MutableThis->ClientPredictionData = new FNetworkPredictionData_Client_Custom(*this);
+			MutableThis->ClientPredictionData->MaxSmoothNetUpdateDist = 92.f;
+			MutableThis->ClientPredictionData->NoSmoothNetUpdateDist = 140.f;
+		}
+	return ClientPredictionData;
+
+}
---
+++ Source/GoeaTechEval/CustomMovementComponent.h
@@ -0,0 +1,57 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "CustomMovementComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GOEATECHEVAL_API UCustomMovementComponent : public UCharacterMovementComponent
+{
+	GENERATED_BODY()
+
+	class FSavedMove_Custom : public FSavedMove_Character
+	{
+	public:
+		typedef FSavedMove_Character Super;
+		/*enum CompressedFlags
+		{
+			FLAG_Sprint = 0x10,
+			FLAG_Dash = 0x20,
+			FLAG_Custom_2 = 0x40,
+			FLAG_Custom_3 = 0x80,
+		};*/
+
+		uint8 Saved_ToClimb : 1;
+
+
+		virtual bool CanCombineWith(const FSavedMovePtr& NewMove, ACharacter* InCharacter, float MaxDelta) const override;
+		virtual void Clear() override;
+		virtual uint8 GetCompressedFlags() const override;
+		virtual void SetMoveFor(ACharacter* C, float InDeltaTime, FVector const& NewAccel, FNetworkPredictionData_Client_Character& ClientData) override;
+		virtual void PrepMoveFor(ACharacter* C) override;
+	};
+
+	bool Safe_ToClimb;
+
+	class FNetworkPredictionData_Client_Custom : public FNetworkPredictionData_Client_Character
+	{
+	public:
+		FNetworkPredictionData_Client_Custom(const UCharacterMovementComponent& ClientMovement);
+
+		typedef FNetworkPredictionData_Client_Character Super;
+
+		virtual FSavedMovePtr AllocateNewMove() override;
+	};
+	
+	public:
+		UCustomMovementComponent();
+
+		virtual FNetworkPredictionData_Client* GetPredictionData_Client() const override;
+
+	
+};
